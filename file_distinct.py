2f5f4c1d7c67af9c775096c5164e04b01816773f
YangYuQing <hiyyq@qq.com>
2021-10-10 01:37:22 +0800

    使用多进程池提升计算速度，性能提升54%

--- file_distinct.py
+++ file_distinct.py
@@ -1,8 +1,10 @@
 import hashlib
 import argparse
 import logging
+import multiprocessing
 import os
 import time
+from typing import Tuple
 
 
 def get_sys_logger():
@@ -18,6 +20,7 @@ def get_sys_logger():
 log = get_sys_logger()
 
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+DEFAULT_CONCURRENT = multiprocessing.cpu_count()
 
 
 def get_file_md5(file_path):
@@ -50,33 +53,46 @@ def parse_args():
                         help='csv file path for output result,default is in current dir,named files_md5.csv')
     parser.add_argument('--format', type=str, default='CSV', choices=['CSV'],
                         help='the output result format,default is CSV,only support CSV now')
+    parser.add_argument('--concurrent', type=int, default=DEFAULT_CONCURRENT,
+                        help=f'the digest worker number,default is cpu count {str(DEFAULT_CONCURRENT)}')
     args = parser.parse_args()
-    return os.path.realpath(args.scan), os.path.realpath(args.output)
+    return os.path.realpath(args.scan), os.path.realpath(args.output), args.concurrent
 
 
-def calculate_dir(scan_dir, result_file_name):
+def calculate_worker(abspath: str):
+    modify_time = os.path.getmtime(abspath)
+    create_time = os.path.getctime(abspath)
+    size = os.path.getsize(abspath)
+    result = get_file_md5(abspath)
+    return '%s,%s,%s,%s,%s,%s,%s' % (abspath, os.path.dirname(abspath), os.path.basename(abspath),
+                                     time.strftime(TIME_FORMAT, time.localtime(modify_time)),
+                                     time.strftime(TIME_FORMAT, time.localtime(create_time)), size, result)
+
+
+def calculate_dir(scan_dir: str, result_file_name: str, concurrent: int):
     start_time = time.time()
     log.info('start scan files in %s, digest result is writing to %s', scan_dir, result_file_name)
+    log.info('current digest worker number is %s', concurrent)
     result_writer = get_result_writer(result_file_name)
-    result_writer.info(
-        'abspath,file_dir,file_name,modify_time,create_time,size(B),digest')
+    result_writer.info('abspath,file_dir,file_name,modify_time,create_time,size(B),digest')
+    worker_pool = multiprocessing.Pool(processes=concurrent)
+
+    def calculate_callback(calculate_result):
+        result_writer.info(calculate_result)
+
     file_count = 0
     for root, dirs, files in os.walk(scan_dir):
         dir_file_count = 0
         for file_name in files:
             file = os.path.join(root, file_name)
-            modify_time = os.path.getmtime(file)
-            create_time = os.path.getctime(file)
-            size = os.path.getsize(file)
-            result = get_file_md5(file)
-            result_writer.info('%s,%s,%s,%s,%s,%s,%s', file, root, file_name,
-                               time.strftime(TIME_FORMAT, time.localtime(modify_time)),
-                               time.strftime(TIME_FORMAT, time.localtime(create_time)), size, result)
+            worker_pool.apply_async(calculate_worker, (file,), callback=calculate_callback)
             file_count += 1
             dir_file_count += 1
         log.info('[%s] files count %s', root, dir_file_count)
-    log.info('done. total files %s. csv result file was written to %s,cost %ss', file_count, result_file_name,
-             (time.time() - start_time))
+    log.info('done scan dir files,total file count %s,waiting worker finish.', file_count)
+    worker_pool.close()
+    worker_pool.join()
+    log.info('all done.result file was written to %s,cost %ss', result_file_name, (time.time() - start_time))
 
 
 if __name__ == '__main__':
